660fa9a2482f53f8c5620d4a217bf4bcb072b46a
Rafael Oliveira <rafah.so@gmail.com>
2017-03-10 22:28:51 -0300

    Added some functions to adjust the canvas in HTML5 in fullscreen

--- Sources/plume/Engine.hx
+++ Sources/plume/Engine.hx
@@ -1,6 +1,7 @@
 package plume;
 
 import kha.System;
+import kha.Display;
 import kha.Framebuffer;
 import kha.Scheduler;
 import kha.Scaler;
@@ -9,6 +10,7 @@ import kha.Image;
 import kha.graphics2.ImageScaleQuality;
 import kha.WindowOptions.Mode;
 import kha.WindowOptions.Position;
+import kha.math.Vector2i;
 import plume.input.Manager;
 
 @:structInit
@@ -21,14 +23,20 @@ class EngineOptions
 	@:optional public var bbHeight:Null<Int>;
 	@:optional public var highQualityScale:Null<Bool>;
 	@:optional public var fullscreen:Null<Bool>;
+	@:optional public var samplesPerPixel:Null<Int>;
 }
 
+@:allow(plume.Plm)
 class Engine
 {
 	static var callback:Void->Void;
 	static var backbuffer:Image;
 	static var highQualityScale:Bool;
 	static var inputs:Array<Manager>;
+
+	#if js
+	static var canvasUsingClientSize:Bool = false;
+	#end
 
 	public static function init(options:EngineOptions, callback:Void->Void):Void
 	{
@@ -62,7 +70,7 @@ class Engine
 
 	inline static function initWindowed(options:EngineOptions)
 	{
-		System.init({ title: options.title, width: options.width, height: options.height }, function () {
+		System.init({ title: options.title, width: options.width, height: options.height, samplesPerPixel: options.samplesPerPixel }, function () {
 			Assets.loadEverything(assetsLoaded);
 		});
 	}
@@ -70,7 +78,8 @@ class Engine
 	inline static function initFullscreen(options:EngineOptions)
 	{
 		System.initEx(options.title,
-			[{ x: Position.Fixed(0), y: Position.Fixed(0), width: options.width, height: options.height, mode: Mode.Fullscreen }],
+			[{ x: Position.Fixed(0), y: Position.Fixed(0), width: options.width, height: options.height, mode: Mode.Fullscreen, 
+			rendererOptions: { samplesPerPixel: options.samplesPerPixel } }], 
 			function(_) {}, function() {
 				Assets.loadEverything(assetsLoaded);
 		});
@@ -91,6 +100,77 @@ class Engine
 			inputs.push(plume.input.GamePad.getManager());*/
 	}
 
+	public static function getSizePrimaryScreen():Vector2i
+	{
+		var size:Vector2i;
+
+		#if js
+		size = new Vector2i(js.Browser.window.innerWidth, js.Browser.window.innerHeight);
+		#else
+		var count = Display.count;
+		var idxPrimary = 0;
+
+		for (i in 0...count)
+		{
+			if (Display.isPrimary(i))
+			{
+				idxPrimary = i;
+				break;
+			}
+		}
+
+		size = new Vector2i(Display.width(idxPrimary), Display.height(idxPrimary));
+		#end
+
+		if (size.x <= 0 || size.y <= 0)
+		{
+			trace('The size of the screen is undefined. Returning a size of 800x600.');
+
+			size.x = 800;
+			size.y = 600;
+		}
+
+		return size;
+	}
+
+	#if js
+	public static function setCanvasToClientSize(canvasName:String = 'khanvas'):Void
+	{		
+		js.Browser.document.body.style.margin = '0px';
+		js.Browser.document.body.style.padding = '0px';
+		js.Browser.document.body.style.height = '100%';
+		js.Browser.document.body.style.overflow = 'hidden';		
+		js.Browser.document.documentElement.style.margin = '0px';
+		js.Browser.document.documentElement.style.padding = '0px';
+		js.Browser.document.documentElement.style.height = '100%';
+		js.Browser.document.documentElement.style.overflow = 'hidden';
+
+		var khanvas:js.html.CanvasElement = kha.SystemImpl.khanvas != null ? 
+			kha.SystemImpl.khanvas : cast js.Browser.document.getElementById(canvasName);
+
+		var w:Int = js.Browser.window.innerWidth;
+		var h:Int = js.Browser.window.innerHeight;
+
+		khanvas.width = w;
+		khanvas.height = h;
+		khanvas.style.width = Std.string(w);
+		khanvas.style.height = Std.string(h);
+
+		canvasUsingClientSize = true;
+	}
+
+	public static function requestCanvasFullScreen():Void
+	{
+		kha.input.Mouse.get().notify(onMouseDownFullscreen, null, null, null, null);
+		//kha.SystemImpl.notifyOfFullscreenChange(Plm.updateWindowSize, null);
+	}
+
+	static function onMouseDownFullscreen(button:Int, x:Int, y:Int):Void
+	{
+		if (!kha.SystemImpl.isFullscreen())		
+			kha.SystemImpl.requestFullscreen();		
+	}
+	#end
 
 	static function assetsLoaded():Void
 	{
--- Sources/plume/Plm.hx
+++ Sources/plume/Plm.hx
@@ -36,6 +36,41 @@ class Plm
 		gameScale = windowWidth / gameWidth;
 	}
 
+	static function updateWindowSize():Void
+	{
+		#if js
+		if (Engine.canvasUsingClientSize)
+		{
+			windowWidth = js.Browser.window.innerWidth;
+			windowHeight = js.Browser.window.innerHeight;
+
+			var khanvas = kha.SystemImpl.khanvas;
+			khanvas.width = windowWidth;
+			khanvas.height = windowHeight;
+			khanvas.style.width = '${windowWidth}px';
+			khanvas.style.height = '${windowHeight}px';
+		}
+		else
+		{
+			windowWidth = System.windowWidth();
+			windowHeight = System.windowHeight();
+		}
+
+		kha.SystemImpl.gl.viewport(0, 0, windowWidth, windowHeight);
+		#else
+		windowWidth = System.windowWidth();
+		windowHeight = System.windowHeight();
+		#end
+
+		if (Engine.backbuffer == null)
+		{
+			gameWidth = windowWidth;
+			gameHeight = windowHeight;
+		}
+		
+		gameScale = windowWidth / gameWidth;
+	}
+
 	public static function addState(state:State, name:String, go:Bool = false):Void
 	{
 		stateList.set(name, state);
@@ -91,5 +126,90 @@ class Plm
 			return true;
 		else
 			return false;
+	}
+
+	/**
+	 * Clamps the value within the minimum and maximum values.
+	 * @param	value		The Float to evaluate.
+	 * @param	min			The minimum range.
+	 * @param	max			The maximum range.
+	 * @return	The clamped value.
+	 */
+	public static function clamp(value:Float, min:Float, max:Float):Float
+	{
+		if (max > min)
+		{
+			if (value < min) return min;
+			else if (value > max) return max;
+			else return value;
+		}
+		else
+		{
+			// Min/max swapped
+			if (value < max) return max;
+			else if (value > min) return min;
+			else return value;
+		}
+	}
+
+	/**
+	 * Linear interpolation between two values.
+	 * @param	a		First value.
+	 * @param	b		Second value.
+	 * @param	t		Interpolation factor.
+	 * @return	When t=0, returns a. When t=1, returns b. When t=0.5, will return halfway between a and b. Etc.
+	 */
+	public static inline function lerp(a:Float, b:Float, t:Float = 1):Float
+	{
+		return a + (b - a) * t;
+	}
+
+	/**
+	 * Linear interpolation between two colors.
+	 * @param	fromColor		First color.
+	 * @param	toColor			Second color.
+	 * @param	t				Interpolation value. Clamped to the range [0, 1].
+	 * return	RGB component-interpolated color value.
+	 */
+	public static inline function colorLerp(fromColor:Int, toColor:Int, t:Float = 1):Int
+	{
+		if (t <= 0)
+		{
+			return fromColor;
+		}
+		else if (t >= 1)
+		{
+			return toColor;
+		}
+		else
+		{
+			var a:Int = fromColor >> 24 & 0xFF,
+				r:Int = fromColor >> 16 & 0xFF,
+				g:Int = fromColor >> 8 & 0xFF,
+				b:Int = fromColor & 0xFF,
+				dA:Int = (toColor >> 24 & 0xFF) - a,
+				dR:Int = (toColor >> 16 & 0xFF) - r,
+				dG:Int = (toColor >> 8 & 0xFF) - g,
+				dB:Int = (toColor & 0xFF) - b;
+			a += Std.int(dA * t);
+			r += Std.int(dR * t);
+			g += Std.int(dG * t);
+			b += Std.int(dB * t);
+			return a << 24 | r << 16 | g << 8 | b;
+		}
+	}
+
+	/**
+	 * Transfers a value from one scale to another scale. For example, scale(.5, 0, 1, 10, 20) == 15, and scale(3, 0, 5, 100, 0) == 40.
+	 * @param	value		The value on the first scale.
+	 * @param	min			The minimum range of the first scale.
+	 * @param	max			The maximum range of the first scale.
+	 * @param	min2		The minimum range of the second scale.
+	 * @param	max2		The maximum range of the second scale.
+	 * @return	The scaled value.
+	 */
+	public inline static function scale(value:Float, min:Float, max:Float, min2:Float, max2:Float):Float
+	{
+		return min2 + ((value - min) / (max - min)) * (max2 - min2);
 	}
 }
